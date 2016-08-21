f1c7ef481c3b78b12f4870212fe920975e36ffbd
suguru <sgr.inoue@gmail.com>
2016-08-22 08:40:00 +0900

    uncomment

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,18 @@ echo "  \_/\_/ \__,_|_|_|\_\____/  \___/|_| |_| |_|  \__,_|\___| .__/|_|\___/ \_
 echo "                                                         | |             __/ |"
 echo "                                                         |_|            |___/ "
 
-# echo uploading static files...
-# aws s3 cp --quiet --recursive ./httpdocs s3://walk30m
-# echo done.
+echo uploading static files...
+aws s3 cp --quiet --recursive ./httpdocs s3://walk30m
+echo done.
 
 echo deploying lambda functions...
 echo 1/2 ipInfo...
-# cd ./lambda && \
-# 	zip -q -r deployment.zip ./ipInfo/* && \
-# 	cdir=`pwd` && \
-# 	aws lambda update-function-code --function-name=ipInfo --zip-file=fileb://${cdir}/deployment.zip && \
-# 	rm deployment.zip
+cd ./lambda && \
+	zip -q -r deployment.zip ./ipInfo/* && \
+	cdir=`pwd` && \
+	aws lambda update-function-code --function-name=ipInfo --zip-file=fileb://${cdir}/deployment.zip && \
+	rm deployment.zip && \
+	cd ../
 
 echo 2/2 createExecutionLog...
 	cd ./lambda/executionLog && \
