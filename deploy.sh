d930761c05fb63c3a6fd7d0159fab704e98c0862
suguru_inoue <sgr.inoue@gmail.com>
2016-08-22 09:52:44 +0900

    uncomment

--- deploy.sh
+++ deploy.sh
@@ -9,23 +9,24 @@ echo "  \_/\_/ \__,_|_|_|\_\____/  \___/|_| |_| |_|  \__,_|\___| .__/|_|\___/ \_
 echo "                                                         | |             __/ |"
 echo "                                                         |_|            |___/ "
 
-# echo uploading static files...
-# aws s3 cp --quiet --recursive ./httpdocs s3://walk30m
-# echo done.
+echo uploading static files...
+aws s3 cp --profile=walk30m --quiet --recursive ./httpdocs s3://walk30m
+echo done.
 
 echo deploying lambda functions...
 echo 1/2 ipInfo...
-# cd ./lambda && \
-# 	zip -q -r deployment.zip ./ipInfo/* && \
-# 	cdir=`pwd` && \
-# 	aws lambda update-function-code --function-name=ipInfo --zip-file=fileb://${cdir}/deployment.zip && \
-# 	rm deployment.zip
+cd ./lambda && \
+	zip -q -r deployment.zip ./ipInfo/* && \
+	cdir=`pwd` && \
+	aws lambda update-function-code --profile=walk30m --function-name=ipInfo --zip-file=fileb://${cdir}/deployment.zip && \
+	rm deployment.zip && \
+	cd ../
 
 echo 2/2 createExecutionLog...
 	cd ./lambda/executionLog && \
 	zip -q -r deployment.zip ./create/* && \
 	cdir=`pwd` && \
-	aws lambda update-function-code --function-name=createExecutionLog --zip-file=fileb://${cdir}/deployment.zip && \
+	aws lambda update-function-code --profile=walk30m --function-name=createExecutionLog --zip-file=fileb://${cdir}/deployment.zip && \
 	rm deployment.zip && \
 	cd ../../
 echo done.
