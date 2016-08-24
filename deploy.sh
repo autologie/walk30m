b3b9f2dc489e108fd7ff53d5d3c3e01b50af899b
suguru <sgr.inoue@gmail.com>
2016-08-24 22:28:56 +0900

    manage IpInfoDB api key as an environment variable

--- deploy.sh
+++ deploy.sh
@@ -29,21 +29,26 @@ aws s3 sync --profile=walk30m --quiet ./tmp s3://walk30m
 rm -rf ./tmp
 echo done.
 
-# echo deploying lambda functions...
-# echo 1/2 ipInfo...
-# cd ./lambda && \
-# 	zip -q -r deployment.zip ./ipInfo/* && \
-# 	cdir=`pwd` && \
-# 	aws lambda update-function-code --profile=walk30m --function-name=ipInfo --zip-file=fileb://${cdir}/deployment.zip && \
-# 	rm deployment.zip && \
-# 	cd ../
-# 
-# echo 2/2 createExecutionLog...
-# 	cd ./lambda/executionLog && \
-# 	zip -q -r deployment.zip ./create/* && \
-# 	cdir=`pwd` && \
-# 	aws lambda update-function-code --profile=walk30m --function-name=createExecutionLog --zip-file=fileb://${cdir}/deployment.zip && \
-# 	rm deployment.zip && \
-# 	cd ../../
-# echo done.
+echo deploying lambda functions...
+echo 1/2 ipInfo...
+cd ./lambda && \
+	mkdir target && \
+	cp -r ./ipInfo ./target/ && \
+	cat ./ipInfo/config.js | sed s/%IP_INFO_DB_KEY%/$IP_INFO_DB_KEY/g > ./target/ipInfo/config.js && \
+	cd ./target && \
+	zip -q -r deployment.zip ./ipInfo/* && \
+	cdir=`pwd` && \
+	aws lambda update-function-code --profile=walk30m --function-name=ipInfo --zip-file=fileb://${cdir}/deployment.zip && \
+	rm deployment.zip && \
+	rm -rf ./target && \
+	cd ../../
+
+echo 2/2 createExecutionLog...
+	cd ./lambda/executionLog && \
+	zip -q -r deployment.zip ./create/* && \
+	cdir=`pwd` && \
+	aws lambda update-function-code --profile=walk30m --function-name=createExecutionLog --zip-file=fileb://${cdir}/deployment.zip && \
+	rm deployment.zip && \
+	cd ../../
+echo done.
 
--- lambda/ipInfo/config.js
+++ lambda/ipInfo/config.js
@@ -1,6 +1,6 @@
 exports.ipInfoDB = {
 	url: 'https://api.ipinfodb.com/v3',
-	key: '',
+	key: '%IP_INFO_DB_KEY%',
 	timeout: 2000,
 	fallbackResponse: { lat: 40.7325795, lng: -73.9740093 }, // new york
 };
