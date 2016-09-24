50386706072de747dafcb3b578399d57db781be1
suguru <sgr.inoue@gmail.com>
2016-09-25 07:43:30 +0900

    refactoring

--- httpdocs/src/js/components/release-note/index.jsx
+++ httpdocs/src/js/components/release-note/index.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { releases } from 'json!yaml!../../../releases.yml';
 
-export default (props) => {
-  const releaseNotes = releases.map(release => {
-    const date = new Date(release.date);
-
-    return (
+export default () => {
+  const releaseNotes = releases
+    .map(r => Object.assign({}, r, {date: new Date(r.date)}))
+    .map(({date, summary}) => (
       <dl key={+date}>
         <dt>{date.getFullYear()}.{date.getMonth() + 1}.{date.getDate()}</dt>
-        <dd dangerouslySetInnerHTML={{__html: release.summary}}></dd>
+        <dd dangerouslySetInnerHTML={{__html: summary}}></dd>
       </dl>
-    );
-  });
+    ));
 
   return (
     <div>
