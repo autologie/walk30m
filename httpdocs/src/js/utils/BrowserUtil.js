c3ddf3e0e333809eb53bd01abeae156cb77ca086
suguru <sgr.inoue@gmail.com>
2016-10-08 15:26:21 +0900

    ignore QuotaError caused by Safari private browsing

--- httpdocs/src/js/utils/BrowserUtil.js
+++ httpdocs/src/js/utils/BrowserUtil.js
@@ -1,10 +1,26 @@
 
+function isSafariPrivateBrowsing() {
+  try {
+    window.localStorage.setItem('test', 'test');
+    window.localStorage.removeItem('test');
+    return false;
+  } catch (ignore) {
+    return true;
+  }
+}
+
 export function isMobile() {
   return window.matchMedia('(orientation: portrait)').matches;
 }
 
 export function save(key, data) {
-  window.localStorage.setItem(key, JSON.stringify(data));
+  try {
+    window.localStorage.setItem(key, JSON.stringify(data));
+  } catch (e) {
+    if (e.name !== 'QuotaExceededError' || !isSafariPrivateBrowsing()) {
+      throw e;
+    }
+  }
 }
 
 export function load(key) {
