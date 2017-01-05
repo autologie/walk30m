eb76dbe7aa33bd3507fc8a36ec3432a3625d7e33
suguru <sgr.inoue@gmail.com>
2017-01-06 04:15:21 +0900

    surpress uglifyjs warnings

--- httpdocs/webpack.config.babel.js
+++ httpdocs/webpack.config.babel.js
@@ -5,31 +5,35 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 module.exports = {
-	context: path.join(__dirname, 'src/'),
-	entry: './js/bootstrap.js',
-	resolve: {
-		root: __dirname,
-	},
-	resolveLoader: {
-		modulesDirectories: [
-			path.join(__dirname, "node_modules"),
-		],
-	},
-	output: {
-		path: path.join(__dirname, '/target'),
-		filename: 'js/app.js',
-	},
-	externals: {
-		'window': 'window',
-		'google': 'google',
-	},
-	module: {
-		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-		],
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin,
+  context: path.join(__dirname, 'src/'),
+  entry: './js/bootstrap.js',
+  resolve: {
+    root: __dirname,
+  },
+  resolveLoader: {
+    modulesDirectories: [
+      path.join(__dirname, "node_modules"),
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '/target'),
+    filename: 'js/app.js',
+  },
+  externals: {
+    'window': 'window',
+    'google': 'google',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
     new InlineEnviromentVariablesPlugin,
     new HtmlWebpackPlugin({
       inject: 'head',
@@ -41,8 +45,8 @@ module.exports = {
       { from: 'images', to: 'images' },
       { from: 'css', to: 'css' }
     ]),
-	],
-	devtool: 'source-map',
+  ],
+  devtool: 'source-map',
   devServer: {
     contentBase: 'target/',
     inline: true,
