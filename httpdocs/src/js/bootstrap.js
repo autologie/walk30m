bd520fef6d8eca3334513d46e7386a01f34fe51a
suguru <sgr.inoue@gmail.com>
2017-01-06 04:03:19 +0900

    disable google analytics at staging environment

--- httpdocs/src/js/bootstrap.js
+++ httpdocs/src/js/bootstrap.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies,import/no-unresolved */
-import './ga';
 import './fb';
 import window from 'window';
 import $ from 'jquery';
 import Application from './Application';
+
+if (process.env.NODE_ENV === 'production') require('./ga');
 
 $(() => new Application($(window.document)));
 
