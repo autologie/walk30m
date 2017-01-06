b0c756fcd34233c4c286113ee962af391ce50a6b
suguru <sgr.inoue@gmail.com>
2017-01-06 17:01:46 +0900

    retab!

--- httpdocs/webpack.config.js
+++ httpdocs/webpack.config.js
@@ -5,35 +5,35 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	context: path.join(__dirname, 'src/'),
-	entry: './js/index.js',
-	resolve: {
-		root: __dirname,
-		extensions: ['.js', '.jsx', '']
-	},
-	resolveLoader: {
-		modulesDirectories: [
-			path.join(__dirname, "node_modules")
-		]
-	},
-	output: {
-		path: path.join(__dirname, '/target'),
-		filename: 'js/app.js',
+  context: path.join(__dirname, 'src/'),
+  entry: './js/index.js',
+  resolve: {
+    root: __dirname,
+    extensions: ['.js', '.jsx', '']
+  },
+  resolveLoader: {
+    modulesDirectories: [
+      path.join(__dirname, "node_modules")
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/target'),
+    filename: 'js/app.js',
     publicPath: '/'
-	},
-	externals: {
-		'window': 'window',
-		'google': 'google'
-	},
-	module: {
-		loaders: [
-			{ test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
-			{ test: /\.css$/, exclude: /node_modules/, loaders: ['style', 'css?modules'] },
-			{ test: /\.yml?$/, exclude: /node_modules/, loader: 'yaml' }
-		]
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin,
+  },
+  externals: {
+    'window': 'window',
+    'google': 'google'
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.css$/, exclude: /node_modules/, loaders: ['style', 'css?modules'] },
+      { test: /\.yml?$/, exclude: /node_modules/, loader: 'yaml' }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin,
     new InlineEnviromentVariablesPlugin,
     new HtmlWebpackPlugin({
       inject: 'head',
@@ -45,8 +45,8 @@ module.exports = {
       { from: 'images', to: 'images' },
       { from: 'css', to: 'css' }
     ])
-	],
-	devtool: 'source-map',
+  ],
+  devtool: 'source-map',
   devServer: {
     contentBase: 'target/',
     inline: true,
