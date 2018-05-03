4a4430657239305445631699e18e1e877f606db3
suguru <sgr.inoue@gmail.com>
2018-05-03 19:36:50 +0900

    refactor

--- backend/execution-logs/index.js
+++ backend/execution-logs/index.js
@@ -11,6 +11,13 @@ function withCors(res) {
     .set("Access-Control-Allow-Methods", "GET,PUT,POST");
 }
 
+function handleNotFound(req, res) {
+  console.warn("Requested resource not found.", req.path);
+  return withCors(res)
+    .status(404)
+    .end();
+}
+
 function handleBadRequest(req, res, message) {
   console.warn("Invalid request.", message);
   return withCors(res)
@@ -31,29 +38,26 @@ function handleCreate(req, res) {
 
   const body = camelcaseKeys(req.body);
   const id = uuidv4();
-  const data = Object.assign({}, body, {
-    startDateTime: new Date(body.startDatetime),
-    userAgent: req.get("User-Agent"),
-    clientIp: req.ip,
-    origin: undefined,
-    isInitial: undefined,
-    startDatetime: undefined,
-    viewport: undefined,
-    originAddress: body.origin.address,
-    originCoordinate: datastore.geoPoint({
-      latitude: body.origin.lat,
-      longitude: body.origin.lng
-    }),
-    viewportWidth: body.viewport.width,
-    viewportHeight: body.viewport.height
-  });
 
   datastore.save(
     {
-      key: datastore.key({
-        path: ["ExecutionLog", id]
-      }),
-      data
+      key: datastore.key(["ExecutionLog", id]),
+      data: Object.assign({}, body, {
+        startDateTime: new Date(body.startDatetime),
+        userAgent: req.get("User-Agent"),
+        clientIp: req.ip,
+        origin: undefined,
+        isInitial: undefined,
+        startDatetime: undefined,
+        viewport: undefined,
+        originAddress: body.origin.address,
+        originCoordinate: datastore.geoPoint({
+          latitude: body.origin.lat,
+          longitude: body.origin.lng
+        }),
+        viewportWidth: body.viewport.width,
+        viewportHeight: body.viewport.height
+      })
     },
     err => {
       if (err) return handleError(req, res, err);
@@ -66,49 +70,43 @@ function handleCreate(req, res) {
 }
 
 function handleUpdate(req, res) {
-  if (typeof req.body !== "object")
-    return handleBadRequest("Request body is empty or insane.");
-
   const pathComponents = req.path.split("/");
   const id = pathComponents[1];
 
-  if (id === undefined || id === "") {
+  if (id === undefined || id === "")
     return handleBadRequest(req, res, "Request path is invalid.");
-  }
 
-  const datastoreKey = datastore.key({
-    path: ["ExecutionLog", id]
-  });
+  if (typeof req.body !== "object")
+    return handleBadRequest("Request body is empty or insane.");
+
+  const datastoreKey = datastore.key(["ExecutionLog", id]);
 
   datastore.get(datastoreKey, (err, entity) => {
     if (err) return handleError(req, res, err);
 
-    if (entity === undefined) {
-      res.status(404).end();
-      return;
-    }
+    if (entity === undefined) return handleNotFound(req, res);
 
-    if (entity.completeDateTime) {
-      console.warn("Attempted to update an entity which has been completed.");
-      res.status(400).end();
-      return;
-    }
+    if (entity.completeDateTime)
+      return handleBadRequest(
+        req,
+        res,
+        "Attempted to update an entity which has been completed."
+      );
 
     const body = camelcaseKeys(req.body);
-    const data = {
-      completeDateTime: new Date(body.completeDatetime),
-      path: (body.resultPath || []).map(coord =>
-        datastore.geoPoint({
-          latitude: coord.lat,
-          longitude: coord.lng
-        })
-      )
-    };
 
     datastore.save(
       {
         key: datastoreKey,
-        data: Object.assign({}, entity, data)
+        data: Object.assign({}, entity, {
+          completeDateTime: new Date(body.completeDatetime),
+          path: (body.resultPath || []).map(coord =>
+            datastore.geoPoint({
+              latitude: coord.lat,
+              longitude: coord.lng
+            })
+          )
+        })
       },
       err => {
         if (err) return handleError(req, res, err);
@@ -126,9 +124,7 @@ exports.executionLogs = (req, res) => {
     if (req.method === "OPTIONS") return withCors(res).status(200).end();
     if (req.method === "POST") return handleCreate(req, res);
     if (req.method === "PUT") return handleUpdate(req, res);
-    return withCors(res)
-      .status(404)
-      .end();
+    return handleNotFound(req, res);
   } catch (e) {
     handleError(req, res, e);
   }
