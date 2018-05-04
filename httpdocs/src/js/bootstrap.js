354d563dc5c9aada345e126661f2b635898e272c
suguru <sgr.inoue@gmail.com>
2018-05-04 11:45:53 +0900

    test pass

--- httpdocs/src/js/bootstrap.js
+++ httpdocs/src/js/bootstrap.js
@@ -2,8 +2,6 @@ import $ from "jquery";
 import "./fb";
 import Application from "./Application";
 
-let a = 3;
-
 if (window.location.origin === process.env.APP_URL) require("./ga");
 
 $(() => new Application($(window.document)));
