63abfe15c39548ea3d2d8765590c5ceaee8a4e49
suguru <sgr.inoue@gmail.com>
2018-05-13 18:30:55 +0900

    users endpoint

---
+++ backend/users/index.js
@@ -0,0 +1,34 @@
+const Datastore = require("@google-cloud/datastore");
+const Users = require("google-function-authorizer");
+
+const datastore = new Datastore();
+
+function handleError(req, res, e) {
+  console.error("An error occurred in handling request.", e);
+  if (!res.headersSent) withCors(res).sendStatus(500);
+}
+
+function withUsers(callback) {
+  return datastore
+    .get(datastore.key(['Miscellaneous', 'secret']))
+    .then(([secret]) => {
+      const users = Users({
+        session: {
+          secret: secret.value,
+        },
+        datastore: {
+          kind: 'User',
+          namespace: undefined,
+        },
+        rules: {
+          create: false,
+        },
+      });
+
+      callback(users);
+    });
+}
+
+exports.users = (req, res) =>
+  withUsers(users => users.handle(req, res))
+    .catch(err => handleError(req, res, err));
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,3 +23,11 @@ module "execution_logs_api" {
   function_name = "executionLogs"
   source_dir    = "../backend/execution-logs"
 }
+
+module "users_api" {
+  source = "modules/cloud_function"
+
+  bucket_name   = "${google_storage_bucket.backend_deploy_bucket.name}"
+  function_name = "users"
+  source_dir    = "../backend/users"
+}
