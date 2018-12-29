03d18ecdd3155e220342fbe14e49a647cd71054f
suguru <sgr.inoue@gmail.com>
2018-12-30 06:44:52 +0900

    init v3

--- httpdocs/src/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-import "./fb";
-import Application from "./Application";
-
-if (window.location.origin === process.env.APP_URL) require("./ga");
-
-$(() => new Application($(window.document)));
--- httpdocs/src/js/config.js
+++
@@ -1,3 +0,0 @@
-export const PUBLIC_API_URL_BASE = process.env.PUBLIC_API_URL_BASE;
-export const EXECUTION_LOG_API_URL = process.env.EXECUTION_LOG_API_URL;
-export const APP_URL = process.env.APP_URL;
